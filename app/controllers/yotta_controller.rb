106bd9d00187d2c954b2cf91b793c48797c384ff
suiyujin <suiyujin04@gmail.com>
2016-01-31 07:01:21 +0900

    post yotta

---
+++ app/controllers/yotta_controller.rb
@@ -0,0 +1,21 @@
+class YottaController < ApplicationController
+  protect_from_forgery except: :create
+  before_action :set_drive, only: :create
+
+  def create
+    latest_run_information_id = @drive.run_informations.pluck(:id).last
+    yottum = Yottum.new(drive_id: @drive.id, run_information_id: latest_run_information_id)
+    if yottum.save
+      User.find(@drive.user_id).send_notification('Yotta!', @drive.id)
+      render text: @drive.id
+    else
+      render text: 'cannot create `yottum`'
+    end
+  end
+
+  private
+
+  def set_drive
+    @drive = Drive.find(params[:drive_id])
+  end
+end
