dc98096caff20b62f11af9543028bcbfaaf9555d
hoshitocat <hoshitocat@gmail.com>
2016-01-31 10:04:14 +0900

    Added respond json latest_drive_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApiBaseController
   before_action :set_user, only: :latest_drive_id
 
   def latest_drive_id
-    render text: @user.drives.pluck(:id).last
+    respond_to do |format|
+      format.text { render text: @user.drives.pluck(:id).last }
+      format.json { render json: { drive_id: @user.drives.pluck(:id).last } }
+    end
   end
 
   private
