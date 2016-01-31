716068e5096773ad6a243e1a765f979c1c14f985
hoshitocat <hoshitocat@gmail.com>
2016-01-31 13:34:49 +0900

    Added starting sidekiq on daemon when deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,6 +51,12 @@ namespace :deploy do
     end
   end
 
+  task :sidekiq_start do
+    on roles(:all) do
+      execute "cd #{current_path} && (RAILS_ENV=#{fetch(:rails_env)} #{fetch(:rbenv_prefix)} bundle exec sidekiq -C config/sidekiq.yml -d)"
+    end
+  end
+
   task :stop do
     on roles(:all) do
       execute "cd #{current_path} && (RAILS_ENV=#{fetch(:rails_env)} #{fetch(:rbenv_prefix)} bundle exec rake unicorn:stop)"
